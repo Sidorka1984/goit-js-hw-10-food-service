0829dbb44ce7b1aaba12fde00022a47db87c77e2
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-04-30 14:33:58 +0300

    Update changeTheme.js

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -8,24 +8,24 @@ const Theme = {
 };
 
 refs.switch.addEventListener('click', onClickEl);
-
+ 
 function onClickEl(e) {
   const check = refs.switch.checked;
 
   if (check) {
     refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
     localStorage.setItem('theme', Theme.DARK)
 
   } else {
-     refs.body.classList.add(Theme.LIGHT);
-    refs.body.classList.remove(Theme.DARK);
+     refs.body.classList.remove(Theme.DARK);
     localStorage.setItem('theme', Theme.LIGHT)
   }
 }
 
-const themeInLocal = localStorage.getItem('theme');
-if (themeInLocal === Theme.DARK) {
-  refs.body.classList.add(Theme.DARK);
-  refs.switch.checked = true;
-}
+const themeInLocal = localStorage.getItem('theme') || Theme.LIGHT
+// if (themeInLocal === Theme.DARK) {
+//   refs.body.classList.add(Theme.DARK);
+//   refs.switch.checked = true;
+// }
+ refs.body.classList.add(themeInLocal);
+ refs.switch.checked = themeInLocal === Theme.DARK;
